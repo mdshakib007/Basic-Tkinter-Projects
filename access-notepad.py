d06fc1b1fdec754fb7ff1e9428abd0e2a082aad5
mdshakib007 <shakibahmed.528874@gmail.com>
2023-04-13 21:28:28 +0600

    second commit

---
+++ access-notepad.py
@@ -0,0 +1,8 @@
+# first we need to import the notepad module
+from tkinter import *
+from OOP_Notepad import NotePaaD
+
+# at this time we need to create an object and access the entire notepad in 4,5 line
+root = Tk()
+NotePaaD(root)
+root.mainloop()
